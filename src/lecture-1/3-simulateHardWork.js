db4f8409290cc908d0f060ccc2f1b6cf04240774
Luke Brobbin <lukemichaelvocalist@gmail.com>
2021-08-23 09:02:34 +0100

    chore: add code example links to examples

--- src/lecture-1/3-simulateHardWork.js
+++ src/lecture-1/3-simulateHardWork.js
@@ -1,3 +1,5 @@
+// http://latentflip.com/loupe/?code=ZnVuY3Rpb24gc2ltdWxhdGVIYXJkV29yayhtaWxsaVNlY29uZHMpewogICAgY29uc3Qgc3RhcnRUaW1lID0gbmV3IERhdGUoKS5nZXRUaW1lKCk7ICAgICAgICAgICAgICAgICAgICAvLyBnZXQgdGhlIGN1cnJlbnQgdGltZQogICAgd2hpbGUgKG5ldyBEYXRlKCkuZ2V0VGltZSgpIDwgc3RhcnRUaW1lICsgbWlsbGlTZWNvbmRzKTsgLy8gaG9nIGNwdSB1bnRpbCB0aW1lJ3MgdXAKfQoKc2ltdWxhdGVIYXJkV29yaygxMDAwKTsKCmNvbnNvbGUubG9nKCJIZWxsbyB3b3JsZCEiKQo%3D!!!
+
 const simulateHardWork = (milliSeconds) => {
   // get the current time
   const startTime = new Date().getTime();
