2bf17bc9ebd82054d1a62a727d18f1e0bad20a0d
Dragoș-Robert N <dragosrobertn@users.noreply.github.com>
2023-06-28 00:31:16 +0100

    Delete readSingleFile.js

--- src/lecture-2/readSingleFile.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-const readFiles = () =>
-  fs.readFile("data/third.json", "utf8", (err, myFile) => {
-    if (err) throw err;
-    console.log(myFile);
-  });
-
-readFiles();
