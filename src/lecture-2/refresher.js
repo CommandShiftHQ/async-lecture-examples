0cfae02de16b1795f564a85761f40f920bf6ff71
Dragoș-Robert N <dragosrobertn@users.noreply.github.com>
2023-06-28 00:31:24 +0100

    Delete refresher.js

--- src/lecture-2/refresher.js
+++
@@ -1,9 +0,0 @@
-// http://latentflip.com/loupe/?code=Y29uc29sZS5sb2coJ0xPRzogQScpCgpzZXRUaW1lb3V0KGZ1bmN0aW9uIGxvZygpIHsKICAgIGNvbnNvbGUubG9nKCdMT0c6IEInKTsKfSwgMCk7Cgpjb25zb2xlLmxvZygnTE9HOiBDJyk7!!!PGJ1dHRvbiBpZD0iaGFyZFdvcmsiPmRvIGhhcmQgd29yazwvYnV0dG9uPgo8YnV0dG9uIGlkPSJoYXBweVRpbWVzIj5oYXBweSB0aW1lcyE8L2J1dHRvbj4%3D
-
-console.log("LOG: A");
-
-setTimeout(function () {
-  console.log("LOG: B");
-}, 0);
-
-console.log("LOG: C");
